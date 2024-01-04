840c939d73b62d8072a11345248347dc80805278
KaysarovMovsar <kaysarovmovsarch@mail.ru>
2024-01-04 16:01:55 +0300

    22_lesson

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Contacts} from "./Contacts";
+
+export const HomePage = () => {
+    return (
+        <div>
+            Contacts
+        </div>
+    );
+};
+
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavLink } from "react-router-dom";
+
+export const Navbar = () => {
+    return (
+        <div className="Navbar">
+            <ul className="nav_list">
+                <li className="nav_item">
+                    <NavLink to="/">Меню</NavLink>
+                </li>
+                <li className="nav_item">
+                    <NavLink to="/about">О нас</NavLink>
+                </li>
+                <li className="nav_item">
+                    <NavLink to="/contacts">Контакты</NavLink>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
